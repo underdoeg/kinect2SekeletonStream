597dc41008f1e5d5c8006ad4549d023f76160790
Philip Whitfield <philip@undef.ch>
2017-06-24 12:50:36 +0200

    bugfix, display rotation

--- sampleClient/main.cpp
+++ sampleClient/main.cpp
@@ -2,6 +2,7 @@
 
 #include <SkeletonStreamClient.h>
 
+
 int main(){
 
 	SkeletonStreamClient client("84.84.84.158");
@@ -11,7 +12,10 @@ int main(){
 
 		if(client.hasSkeleton()){
 			auto pos = client.getJointPosition(JointID::HandLeft);
-			std::cout << "LEFT HAND: " << pos.x << ", " << pos.y << ", " << pos.z << std::endl;
+			auto orientation = client.getJointOrientation(JointID::WristLeft);
+			auto eulers = glm::eulerAngles(orientation);
+			std::cout << "LEFT HAND POS: " << pos.x << ", " << pos.y << ", " << pos.z << std::endl;
+			//std::cout << "LEFT WRIST EULERS: " << eulers.x << ", " << eulers.y << ", " << eulers.z << std::endl;
 		}
 	}
 
